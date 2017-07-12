3b3069b577981a707aed3c8dee0bb1befde9b07e
Matthew Dillon <matthewrdillon@gmail.com>
2017-07-11 23:27:00 -0800

    ENH: Set up Djoser (#23)
    Fixes #20

--- config/settings/base.py
+++ config/settings/base.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + LOCAL_APPS
 MIDDLEWARE = (
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -159,3 +160,8 @@ REST_FRAMEWORK = {
 }
 
 SITE_ID = 1
+
+DJOSER = {
+    'SITE_NAME': 'CCDB (test)',
+    'DOMAIN': 'localhost:4200',
+}
